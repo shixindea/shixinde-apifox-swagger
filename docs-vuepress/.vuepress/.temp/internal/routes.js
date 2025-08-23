36ff5957cbb0851ab68a785a54874010c68d68df
袁果锅 <2914310412@qq.com>
2025-08-23 21:35:05 +0800

    update docs

--- docs-vuepress/.vuepress/.temp/internal/routes.js
+++ docs-vuepress/.vuepress/.temp/internal/routes.js
@@ -9,7 +9,24 @@ export const routes = Object.fromEntries([
   ["/quick-start.html", { loader: () => import(/* webpackChunkName: "quick-start.html" */"/Users/guoguo/code/my-project/shixinde-apifox-swagger/docs-vuepress/.vuepress/.temp/pages/quick-start.html.js"), meta: {"title":"快速开始"} }],
   ["/troubleshooting.html", { loader: () => import(/* webpackChunkName: "troubleshooting.html" */"/Users/guoguo/code/my-project/shixinde-apifox-swagger/docs-vuepress/.vuepress/.temp/pages/troubleshooting.html.js"), meta: {"title":"故障排除"} }],
   ["/typescript.html", { loader: () => import(/* webpackChunkName: "typescript.html" */"/Users/guoguo/code/my-project/shixinde-apifox-swagger/docs-vuepress/.vuepress/.temp/pages/typescript.html.js"), meta: {"title":"TypeScript 类型安全工具"} }],
-  ["/en/", { loader: () => import(/* webpackChunkName: "en_index.html" */"/Users/guoguo/code/my-project/shixinde-apifox-swagger/docs-vuepress/.vuepress/.temp/pages/en/index.html.js"), meta: {"title":"Hello VuePress"} }],
   ["/theme-reco/%E5%BF%AB%E9%80%9F%E4%B8%8A%E6%89%8B.html", { loader: () => import(/* webpackChunkName: "theme-reco_快速上手.html" */"/Users/guoguo/code/my-project/shixinde-apifox-swagger/docs-vuepress/.vuepress/.temp/pages/theme-reco/快速上手.html.js"), meta: {"title":"快速上手"} }],
+  ["/en/", { loader: () => import(/* webpackChunkName: "en_index.html" */"/Users/guoguo/code/my-project/shixinde-apifox-swagger/docs-vuepress/.vuepress/.temp/pages/en/index.html.js"), meta: {"title":"Hello VuePress"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/guoguo/code/my-project/shixinde-apifox-swagger/docs-vuepress/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateRoutes) {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+  }
+  if (__VUE_HMR_RUNTIME__.updateRedirects) {
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
--- docs-vuepress/.vuepress/.temp/internal/siteData.js
+++ docs-vuepress/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,14 @@
-export const siteData = JSON.parse("{\"base\":\"/shixinde-apifox-swagger/\",\"lang\":\"zh-CN\",\"title\":\"@shixinde/apifox-swagger\",\"description\":\"袁果锅开发工具类\",\"head\":[[\"link\",{\"rel\":\"icon\",\"href\":\"/favicon.ico\"}]],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/shixinde-apifox-swagger-docs/\",\"lang\":\"zh-CN\",\"title\":\"@shixinde/apifox-swagger\",\"description\":\"袁果锅开发工具类\",\"head\":[[\"link\",{\"rel\":\"icon\",\"href\":\"/favicon.ico\"}]],\"locales\":{}}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateSiteData) {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ siteData }) => {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  })
+}
